5beee2f056cf6d42cdc9f7f5c2b91ff489f9b6f9
duyduyomm <dodiforgame@gmail.com>
2024-12-24 00:48:13 +0700

    fix image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,6 +54,7 @@ export default function Home() {
               width: "100%",
               height: "auto",
             }}
+            layout="responsive"
           />
         </div>
       </div>
